da298d4f77f4614dfceb2303c5b4944cefd2f1a9
yiming-fang <yf2484@columbia.edu>
2023-02-06 18:46:39 -0500

    refactor

--- src/BruteForce.hs
+++ src/BruteForce.hs
@@ -2,26 +2,24 @@ module BruteForce (
   solve,
 ) where
 
-moves :: [(Int -> Int, String)]
+type FuncList = [(Int -> Int, String)]
+
+moves :: FuncList
 moves = possibleMoves (zip ops opNames) nums
  where
   nums = [1 .. 10]
   ops = map flip [(+), (-), (*), div]
   opNames = ["+", "-", "*", "/"]
-
-searchSpace :: [(Int -> Int, String)] -> [(Int -> Int, String)]
-searchSpace space = space ++ searchSpace (cartesian space)
-
-possibleMoves :: [(Int -> Int -> Int, String)] -> [Int] -> [(Int -> Int, String)]
-possibleMoves fs ns = [(f n, getName n name) | (f, name) <- fs, n <- ns]
- where
+  possibleMoves fs ns = [(f n, getName n name) | (f, name) <- fs, n <- ns]
   getName n o = "(" ++ o ++ " " ++ show n ++ ")"
 
-cartesian :: [(Int -> Int, String)] -> [(Int -> Int, String)]
+searchSpace :: FuncList -> FuncList
+searchSpace space = space ++ searchSpace (cartesian space)
+
+cartesian :: FuncList -> FuncList
 cartesian fs = [(f2 . f1, n2 ++ " . " ++ n1) | (f1, n1) <- fs, (f2, n2) <- moves]
 
-solve :: [(Int, Int)] -> [(Int -> Int, String)]
-solve pairs = filter checkPairs (searchSpace moves)
+solve :: [(Int, Int)] -> FuncList
+solve pairs = filter checkPairs $ searchSpace moves
  where
-  checkPairs (f, _) = all (checkPair f) pairs
-  checkPair f (i, o) = f i == o
+  checkPairs (f, _) = all (\(i, o) -> f i == o) pairs
