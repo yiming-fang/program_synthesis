0e5d87465da0d4cc6275038722f05d4879d089da
yiming-fang <yf2484@columbia.edu>
2023-02-06 18:34:00 -0500

    replace ugly hardcode with infinite recursion

--- src/BruteForce.hs
+++ src/BruteForce.hs
@@ -1,37 +1,27 @@
 module BruteForce (
-    solve,
+  solve,
 ) where
-
-nums :: [Int]
-nums = [1 .. 10]
-
-opNames :: [String]
-opNames = ["+ ", "- ", "* ", "/ "]
-
-ops :: [Int -> Int -> Int]
-ops = map flip [(+), (-), (*), div]
 
 moves :: [(Int -> Int, String)]
 moves = possibleMoves (zip ops opNames) nums
+ where
+  nums = [1 .. 10]
+  ops = map flip [(+), (-), (*), div]
+  opNames = ["+", "-", "*", "/"]
 
-searchSpace :: [(Int -> Int, String)]
-searchSpace = level0 ++ level1 ++ level2 ++ level3
-   where
-    level0 = moves
-    level1 = cartesian level0
-    level2 = cartesian level1
-    level3 = cartesian level2
+searchSpace :: [(Int -> Int, String)] -> [(Int -> Int, String)]
+searchSpace space = space ++ searchSpace (cartesian space)
 
 possibleMoves :: [(Int -> Int -> Int, String)] -> [Int] -> [(Int -> Int, String)]
 possibleMoves fs ns = [(f n, getName n name) | (f, name) <- fs, n <- ns]
-  where
-    getName n o = "(" ++ o ++ show n ++ ")"
+ where
+  getName n o = "(" ++ o ++ " " ++ show n ++ ")"
 
 cartesian :: [(Int -> Int, String)] -> [(Int -> Int, String)]
 cartesian fs = [(f2 . f1, n2 ++ " . " ++ n1) | (f1, n1) <- fs, (f2, n2) <- moves]
 
 solve :: [(Int, Int)] -> [(Int -> Int, String)]
-solve pairs = filter checkPairs searchSpace
-  where
-    checkPairs (f, _) = foldr ((&&) . checkPair f) True pairs
-    checkPair f (i, o) = f i == o
+solve pairs = filter checkPairs (searchSpace moves)
+ where
+  checkPairs (f, _) = all (checkPair f) pairs
+  checkPair f (i, o) = f i == o
